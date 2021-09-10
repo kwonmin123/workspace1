fa12031353d81d2728d004aecac2a07fbc06045e
kwonmin123123 <zzacjk@naver.com>
2021-09-10 18:21:42 +0900

    20210910

--- src/regex01/MainClass.java
+++ src/regex01/MainClass.java
@@ -2,17 +2,41 @@ package regex01;
 
 import java.util.regex.Pattern;
 
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//		boolean front=true;
+//		String s1="[a-z&&[^c]]";
+//		String s2="[a-z[A-Z][0-9]_]";
+//		String s3="a{3}";
+//		String s4="aaa";
+//		System.out.println(s3);
+//		System.out.println(Pattern.matches("\\w*b\\wb\\w*","str"));
+//		s1.charAt(0);
+//		s1.replaceAll("x", "");
+//		
+//		String bb="bb";
+//		bb+="b";
+//		
+//		System.out.println(Pattern.matches(s2, "D"));
+//		
+		String s1= "xhappy";
+		String s2= "hxhappy";
+		String s3= "xx";
+		String s4= "x";
+		String s5= "";
 		
-		String s1="[a-z&&[^c]]";
-		String s2="[a-z[A-Z][0-9]_]";
+		System.out.println(s2.substring(0,2).replaceAll("x", ""));
 		
 		
 		
-		System.out.println(Pattern.matches(s2, "D"));
+		
+		
+		
+		
+	
 		}
 
 }
