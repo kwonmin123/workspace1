c70c57eb071d42715bd2a41e12aff44fbab375ad
kwonmin123123 <zzacjk@naver.com>
2021-09-22 22:20:10 +0900

    Update KthNum.java

--- src/kthNum/KthNum.java
+++ src/kthNum/KthNum.java
@@ -15,14 +15,14 @@ public class KthNum {
 	    	   temp[j]=array[commands[i][0]-1+j];
 	       }
 
-//	       Arrays.sort(temp);
-	       for(int j=0;j<temp.length;j++) {
-	    	   for(int k=1 ;j+k<temp.length;k++) {
-	    		   temp[j]=Math.min(temp[j], temp[j+k]);
-	    		   temp[j+k]=Math.max(temp[j], temp[j+k]);
-	    	   
-	    	   }
-	       }
+	       Arrays.sort(temp);
+//	       for(int j=0;j<temp.length;j++) {
+//	    	   for(int k=1 ;j+k<temp.length;k++) {
+//	    		   temp[j]=Math.min(temp[j], temp[j+k]);
+//	    		   temp[j+k]=Math.max(temp[j], temp[j+k]);
+//	    	   
+//	    	   }
+//	       }
 	       
 	       answer[i]=temp[commands[i][2]-1];       	
 	        	 }
