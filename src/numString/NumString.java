0c6977eb6b45c7263a34c574faf7c7244ba94a6b
kwonmin123123 <zzacjk@naver.com>
2021-09-22 22:26:33 +0900

    Create NumString.java

---
+++ src/numString/NumString.java
@@ -0,0 +1,21 @@
+package numString;
+
+public class NumString {
+
+
+	    public int solution(String s) {
+	        
+	        s = s.replaceAll("one"   , "1");
+	        s = s.replaceAll("two"   , "2");
+	        s = s.replaceAll("three" , "3");
+	        s = s.replaceAll("four"  , "4");
+	        s = s.replaceAll("five"  , "5");
+	        s = s.replaceAll("six"   , "6");
+	        s = s.replaceAll("seven" , "7");
+	        s = s.replaceAll("eight" , "8");
+	        s = s.replaceAll("nine"  , "9");
+	        s = s.replaceAll("zero"  , "0");
+	        
+	        return Integer.parseInt(s);
+	    }
+}
